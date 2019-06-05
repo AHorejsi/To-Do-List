871b4ade0e0aa9c5b59944e963718c6cd15f3e5e
AHorejsi <alex.horejsi59@gmail.com>
2019-06-05 08:30:05 -0700

    Implemented user type

--- public/javascripts/Task.js
+++ public/javascripts/Task.js
@@ -1,1 +1,10 @@
-﻿
+﻿"use strict";
+
+
+function Task(description, timeToSendEmail) {
+    this.description = description;
+    this.timeToSendEmail = timeToSendEmail;
+}
+
+
+module.exports = Task;
