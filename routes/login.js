bda44455f62422d39d942f6beee184ddf0df605e
AHorejsi <alex.horejsi59@gmail.com>
2019-06-05 07:14:44 -0700

    Initial commit

---
+++ routes/login.js
@@ -0,0 +1,13 @@
+﻿"use strict";
+
+
+const express = require("express");
+const router = express.Router();
+
+
+router.get("/login", (request, response) => {
+    response.render("login");
+});
+
+
+module.exports = router;
