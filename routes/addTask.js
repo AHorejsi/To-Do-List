68ad6e7e5aaacc0f437877e9cb183e225a616add
AHorejsi <alex.horejsi59@gmail.com>
2019-06-20 16:56:05 -0700

    Adding new files

---
+++ routes/addTask.js
@@ -0,0 +1,34 @@
+﻿"use strict";
+
+const express = require("express");
+const mongodb = require("mongodb");
+const _ = require("lodash");
+
+
+const router = express.Router();
+const MongoClient = mongodb.MongoClient;
+const ObjectId = mongodb.ObjectId;
+
+router.post("/", (request, response) => {
+    MongoClient.connect("mongodb://localhost:27017/", { useNewUrlParser: true }, (err, db) => {
+        if (err) {
+            response.redirect("error");
+            return;
+        }
+
+        _.mapValues(request.body, _.trim);
+
+        let database = db.db("database");
+
+        database.collection("tasks").insertOne({
+            userId: ObjectId(request.cookies.userId),
+            description: request.body.newTaskText
+        });
+
+        response.redirect("mainPage");
+        db.close();
+    });
+});
+
+
+module.exports = router;
---
+++ routes/editTask.js
@@ -0,0 +1,36 @@
+﻿"use strict";
+
+const express = require("express");
+const mongodb = require("mongodb");
+
+
+const router = express.Router();
+const MongoClient = mongodb.MongoClient;
+const ObjectId = mongodb.ObjectId;
+
+router.post("/", (request, response) => {
+    MongoClient.connect("mongodb://localhost:27017/", { useNewUrlParser: true }, (err, db) => {
+        if (err) {
+            response.redirect("error");
+            return;
+        }
+
+        let database = db.db("database");
+        let update = {
+            $set: {
+                description: request.body.taskText
+            }
+        };
+
+        database.collection("tasks").updateOne({ _id: ObjectId(request.query.taskId) }, update).then((result) => {
+            response.redirect("mainPage");
+        }).catch((error) => {
+            response.redirect("error");
+        }).finally(() => {
+            db.close();
+        })
+    });
+});
+
+
+module.exports = router;
